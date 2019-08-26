44b7d278548fdb2838281285226e7f1c814fc24b
Wayne <wang@dxc.com>
2019-08-26 15:13:56 +0800

    controller

--- frye/src/main/java/com/qhc/frye/rest/controller/qhc/MaterialController.java
+++ frye/src/main/java/com/qhc/frye/rest/controller/qhc/MaterialController.java
@@ -3,14 +3,24 @@
  */
 package com.qhc.frye.rest.controller.qhc;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.qhc.frye.service.ConfirugrableService;
+import com.qhc.frye.rest.controller.entity.Material;
+import com.qhc.frye.service.MaterialService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * @author wang@dxc.com
@@ -22,6 +32,14 @@ import io.swagger.annotations.Api;
 public class MaterialController {
 	
 	@Autowired
-    private ConfirugrableService configService;
+    private MaterialService configService;
+	
+	 @ApiOperation(value="push a new order to SAP")
+	  @PostMapping(value = "/new",produces = "application/json;charset=UTF-8")
+	  @ResponseStatus(HttpStatus.OK)
+	  public void update(@RequestBody(required=true) @Valid List<Material> material) throws Exception
+	  {
+		 
+	  }
 
 }
--- frye/src/main/java/com/qhc/frye/rest/controller/qhc/OrderController.java
+++ frye/src/main/java/com/qhc/frye/rest/controller/qhc/OrderController.java
@@ -5,17 +5,19 @@ package com.qhc.frye.rest.controller.qhc;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qhc.frye.rest.controller.entity.Order;
-import com.qhc.frye.service.ConfirugrableService;
+import com.qhc.frye.service.MaterialService;
 import com.qhc.frye.service.OrderService;
 
 import io.swagger.annotations.Api;
@@ -31,35 +33,36 @@ import io.swagger.annotations.ApiOperation;
 public class OrderController {
 	
 	@Autowired
-    private ConfirugrableService configService;
+    private MaterialService configService;
 	
 	@Autowired
 	private OrderService orderService;
 	
-	@ApiOperation(value="Get material by configurable", notes="Get material by configurable Json string")
-    @GetMapping(value = "/Materinal/{configurable}")
+	@ApiOperation(value="Submit Order by sales", notes="Save&Submit to headQuanter by sales")
+    @GetMapping(value = "submit")
     @ResponseStatus(HttpStatus.OK)
-    public void submit(@PathVariable("configurable") List<String> configurable) throws Exception
+    public void submit(@RequestBody(required=true) @Valid Order order) throws Exception
     {	
 		
-		configService.findMaterial(configurable);
+		
     }
 	
-	@ApiOperation(value="Looking for the newest order", notes="Looking fro the newest order from cloud")
-    @GetMapping(value = "/order/{configurable}")
+	@ApiOperation(value="Save order as draft", notes="Looking fro the newest order from cloud")
+    @GetMapping(value = "save")
     @ResponseStatus(HttpStatus.OK)
-    public void  save() throws Exception
+    public void  save(@RequestBody(required=true) @Valid Order order) throws Exception
     {	
 		
 		orderService.findNewestOrder();
     }
+	
 	@ApiOperation(value="Looking for the newest order", notes="Looking fro the newest order from cloud")
-    @GetMapping(value = "/order/{configurable}")
+    @GetMapping(value = "update")
     @ResponseStatus(HttpStatus.OK)
-    public Order getByCode() throws Exception
+    public void  update(@RequestBody(required=true) @Valid Order order) throws Exception
     {	
 		
-		return new Order();
+		
     }
 	
 
