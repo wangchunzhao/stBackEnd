1d40a1e5b515d279c69507f4191c0adff58fd895
lizuoshan1028 <lizuoshan1028@163.com>
2019-08-29 08:50:17 +0800

    add

--- frye/src/main/java/com/qhc/frye/dao/RoleRepository.java
+++ frye/src/main/java/com/qhc/frye/dao/RoleRepository.java
@@ -1,5 +1,7 @@
 package com.qhc.frye.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,8 @@ import com.qhc.frye.domain.Role;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+	List<Role> findByNameLike(String name);
 
 
 
--- frye/src/main/java/com/qhc/frye/dao/UserRepository.java
+++ frye/src/main/java/com/qhc/frye/dao/UserRepository.java
@@ -1,5 +1,7 @@
 package com.qhc.frye.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,12 @@ import com.qhc.frye.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+	List<User> findByUserMailLikeAndIsActive(String userMail, Integer isActive);
+
+	List<User> findByUserMailLike(String userMail);
+
+	List<User> findByIsActive(Integer isActive);
 
 
 
