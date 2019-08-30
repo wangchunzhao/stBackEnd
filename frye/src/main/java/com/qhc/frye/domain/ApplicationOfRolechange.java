761d6b8c8f9c48a45c101705990e1b27c3e0095e
lizuoshan1028 <lizuoshan1028@163.com>
2019-08-30 15:35:31 +0800

    update

--- frye/src/main/java/com/qhc/frye/domain/ApplicationOfRolechange.java
+++ frye/src/main/java/com/qhc/frye/domain/ApplicationOfRolechange.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -54,8 +55,6 @@ public class ApplicationOfRolechange implements Serializable{
 	@Column(name="b_roles_id",length = 10)
     private Integer bRolesId;
 	
-
-    
 
     public String getCreator() {
         return creator;
