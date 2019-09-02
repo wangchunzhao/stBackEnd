161d8ece69f55bcfec1d4e098790e17827af6a08
Wayne <wang@dxc.com>
2019-09-02 14:27:20 +0800

    update of frye

--- frye.sql
+++ frye.sql
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
--- MySQL Workbench Forward Engineering
-
+﻿
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -3680,5 +3678,3 @@ INSERT INTO `bohemian`.`sap_item_category_plan` (`code`, `name`, `is_reserved`, 
 
 COMMIT;
 
->>>>>>> branch 'sprint3' of https://github.com/dxc-x/frye.git
->>>>>>> refs/remotes/origin/sprint3
