4362ccd18ae3a6e0636c374f596f37f5ed8f343d
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-05 14:36:04 +0800

    update

--- frye/src/main/java/com/qhc/frye/dao/ParameterSettingsRepository.java
+++ frye/src/main/java/com/qhc/frye/dao/ParameterSettingsRepository.java
@@ -12,6 +12,7 @@ import com.qhc.frye.domain.Parameter;
 @Repository
 public interface ParameterSettingsRepository extends JpaRepository<Parameter, Integer>{
 
+	//当前生效信息
 	@Query(value="select * from b_settings where code=?1 and enable_date<= curdate() limit 1;" ,nativeQuery=true)
 	Parameter findEnabledInfo(String code);
 	
