c9e66ef52c2b5d3f92553f2fab246fc4ba099c95
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-06 09:08:16 +0800

    update

--- frye/src/main/java/com/qhc/frye/domain/CustomerAffiliation.java
+++ frye/src/main/java/com/qhc/frye/domain/CustomerAffiliation.java
@@ -5,6 +5,9 @@ package com.qhc.frye.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -15,11 +18,16 @@ import javax.validation.constraints.NotNull;
 @Entity
 @Table(name = "sap_industry_and_customer")
 public class CustomerAffiliation {
+	@Id
 	@NotNull
-	@Column(name = "name",columnDefinition="TEXT")
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	@Column(name = "sap_industry_code",length = 4)
 	private String industryCode;
 	@NotNull
-	@Column(name = "name",columnDefinition="TEXT")
+	@Column(name = "sap_customer_code",length = 16)
 	private String customerCode;
 	
 	
