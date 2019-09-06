955670e314309709ddce9bf502186de505ea9155
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-06 09:57:03 +0800

    update

--- frye/src/main/java/com/qhc/frye/rest/controller/qhc/OperationController.java
+++ frye/src/main/java/com/qhc/frye/rest/controller/qhc/OperationController.java
@@ -1,6 +1,9 @@
 package com.qhc.frye.rest.controller.qhc;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qhc.frye.domain.ApplicationOfRolechange;
 import com.qhc.frye.domain.Operations;
+import com.qhc.frye.domain.User;
+import com.qhc.frye.service.ApplicationOfRolechangeService;
 import com.qhc.frye.service.OperationService;
+import com.qhc.frye.service.UserService;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -25,6 +34,10 @@ public class OperationController {
 	
 	@Autowired
 	private OperationService operationService;
+	@Autowired 
+	private ApplicationOfRolechangeService appService;
+	@Autowired 
+	private UserService userService;
 	
 	@ApiOperation(value="Query operations", notes="Query operations")
 	@GetMapping(value = "/operations")
@@ -41,6 +54,27 @@ public class OperationController {
     {	
 		return operationService.findById(id);
     }
+	
+	@ApiOperation(value=" Query operations by user id", notes="Query operations by user id")
+	@GetMapping(value = "/operations/user/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public User findById(@PathVariable("id") int id) throws Exception
+	{	
+		//得到application信息
+			List<ApplicationOfRolechange> apps = appService.findByBUsersId(id);
+			
+			User user = userService.findById(id);
+			boolean flag =false;
+			Set<ApplicationOfRolechange> set = new HashSet<ApplicationOfRolechange>();
+			if(apps!=null&&apps.size()>0) {
+				for(ApplicationOfRolechange app :apps) {
+					set.add(app);
+				}
+				
+			}
+			user.setApps(set);
+			return user;
+	}
 
 	
 
