bb8cbc8b882be364d79f9f9dd36564974e1475f6
wangch <chunzhao.wang@carrier.com>
2019-09-09 12:39:05 +0800

    update 1

--- frye/src/main/java/com/qhc/frye/dao/SapSalesOfficeRepository.java
+++ frye/src/main/java/com/qhc/frye/dao/SapSalesOfficeRepository.java
@@ -1,12 +1,13 @@
 package com.qhc.frye.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.qhc.frye.domain.SapSalesOffice;
 
 @Repository
-public interface SapSalesOfficeRepository extends JpaRepository<SapSalesOffice, Integer> {
+public interface SapSalesOfficeRepository extends JpaRepository<SapSalesOffice, Integer> ,JpaSpecificationExecutor<SapSalesOffice>{
 
 	SapSalesOffice findByCode(String code);
 	
