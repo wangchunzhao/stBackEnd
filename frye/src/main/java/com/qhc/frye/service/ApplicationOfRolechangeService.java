feff1619384de625a0282ae3383da08d1d8ad34a
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-09 21:27:40 +0800

    update

--- frye/src/main/java/com/qhc/frye/service/ApplicationOfRolechangeService.java
+++ frye/src/main/java/com/qhc/frye/service/ApplicationOfRolechangeService.java
@@ -42,55 +42,58 @@ public class ApplicationOfRolechangeService {
 			for(ApplicationOfRolechange app : apps) {
 				roles= roles+app.getbRolesId()+",";
 			}
+			//角色发生变化的话，删除重加；否则，直接修改
+			//前端传来的变更后的角色列表
+			List<Role> roleList = user.getRoles();
+			if(roleList!=null&&roleList.size()>0) {
+				String roleIds = "";
+				//修改人保存的位置，得到修改人
+				ApplicationOfRolechange creatorContainer = user.getApps().get(0);
+				String creator = creatorContainer.getCreator();
+				//得到所有的角色
+				for(Role r:roleList) {
+					roleIds= roleIds+r.getId()+",";
+				}
+				
+				if(roleIds.equals(roles)) {
+					//直接修改数据不变更角色数据
+					for(ApplicationOfRolechange app : apps) {
+						app.setApprovalTime(new Date());
+						app.setAttachedCode(user.getRegion().getCode());
+						app.setCreator(creator);
+						app.setApprovalTime(new Date());
+						app.setCreateTime(new Date());
+						applicationRepository.save(app);
+					}
+				}else {
+					deleteByBUsersId(user.getId());
+					applicationRepository.flush();
+					//得到角色id
+					for(Role r:roleList) {
+						ApplicationOfRolechange app = new ApplicationOfRolechange();
+						app.setApprovalTime(new Date());
+						app.setBusersId(user.getId());
+						app.setIsactive(user.getIsActive());
+						app.setCreateTime(new Date());
+						app.setCreator(creator);
+						app.setAttachedCode(user.getRegion().getCode());
+						app.setbRolesId(r.getId());
+						if(oldApp!=null) {
+							app.setApproverFact(oldApp.getApproverFact());
+							app.setApproverRequired(oldApp.getApproverRequired());
+						}else {
+							app.setApproverFact("none");
+							app.setApproverRequired("none");
+						}
+						applicationRepository.save(app);
+					}
+				}
+			}
+		}else {
+			//新增用户关系
+			
 		}
 		
-		//角色发生变化的话，删除重加；否则，直接修改
-		//前端传来的变更后的角色列表
-		List<Role> roleList = user.getRoles();
-		if(roleList!=null&&roleList.size()>0) {
-			String roleIds = "";
-			//修改人保存的位置，得到修改人
-			ApplicationOfRolechange creatorContainer = user.getApps().get(0);
-			String creator = creatorContainer.getCreator();
-			//得到所有的角色
-			for(Role r:roleList) {
-				roleIds= roleIds+r.getId()+",";
-			}
-			
-			if(roleIds.equals(roles)) {
-				//直接修改数据不变更角色数据
-				for(ApplicationOfRolechange app : apps) {
-					app.setApprovalTime(new Date());
-					app.setAttachedCode(user.getRegion().getCode());
-					app.setCreator(creator);
-					app.setApprovalTime(new Date());
-					app.setCreateTime(new Date());
-					applicationRepository.save(app);
-				}
-			}else {
-				deleteByBUsersId(user.getId());
-				applicationRepository.flush();
-				//得到角色id
-				for(Role r:roleList) {
-					ApplicationOfRolechange app = new ApplicationOfRolechange();
-					app.setApprovalTime(new Date());
-					app.setBusersId(user.getId());
-					app.setIsactive(user.getIsActive());
-					app.setCreateTime(new Date());
-					app.setCreator(creator);
-					app.setAttachedCode(user.getRegion().getCode());
-					app.setbRolesId(r.getId());
-					if(oldApp!=null) {
-						app.setApproverFact(oldApp.getApproverFact());
-						app.setApproverRequired(oldApp.getApproverRequired());
-					}else {
-						app.setApproverFact("none");
-						app.setApproverRequired("none");
-					}
-					applicationRepository.save(app);
-				}
-			}
-		}
 		
 		
 	}
