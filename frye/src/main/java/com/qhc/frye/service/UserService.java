5a09fc5eedfa3321f609a5e80c037020be4df68a
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-10 10:43:11 +0800

    update

--- frye/src/main/java/com/qhc/frye/service/UserService.java
+++ frye/src/main/java/com/qhc/frye/service/UserService.java
@@ -1,16 +1,11 @@
-/**
- * 
- */
 package com.qhc.frye.service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
