b5f7696f1af1fb8c999cc684d5b77bcddf9f8c0e
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-23 18:38:53 +0800

    add

---
+++ frye/src/main/java/com/qhc/frye/service/SpecialDeliveryService.java
@@ -0,0 +1,29 @@
+package com.qhc.frye.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.qhc.frye.dao.SpecialDeliveryRepository;
+import com.qhc.frye.domain.SpecialDelivery;
+
+@Service
+public class SpecialDeliveryService {
+
+	@Autowired
+	private SpecialDeliveryRepository specialDeliveryRepository;
+
+
+
+	public SpecialDelivery saveOrUpdate(SpecialDelivery sd) {
+		
+		return specialDeliveryRepository.save(sd);
+	}
+
+	public List<SpecialDelivery> findByOrdersId(Integer kOrdersId) {
+		return specialDeliveryRepository.findByOrdersId(kOrdersId);
+	}
+
+	public SpecialDelivery findById(Integer applyId) {
+		return specialDeliveryRepository.getOne(applyId);
+	}
+}
