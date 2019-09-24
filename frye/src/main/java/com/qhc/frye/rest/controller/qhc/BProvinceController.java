d91999c7f847a4494c7d27ee2e3710cce9e600a3
wangch <chunzhao.wang@carrier.com>
2019-09-24 16:02:43 +0800

    update 2

--- frye/src/main/java/com/qhc/frye/rest/controller/qhc/BProvinceController.java
+++ frye/src/main/java/com/qhc/frye/rest/controller/qhc/BProvinceController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.qhc.frye.domain.BCityFreight;
 import com.qhc.frye.domain.BProvince;
 import com.qhc.frye.service.BProvinceService;
 
