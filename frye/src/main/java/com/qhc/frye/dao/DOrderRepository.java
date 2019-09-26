2011a13a0e4a4b3e3b32de233fa1f887ddc4e6a0
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-26 16:18:37 +0800

    update

---
+++ frye/src/main/java/com/qhc/frye/dao/DOrderRepository.java
@@ -0,0 +1,11 @@
+package com.qhc.frye.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import com.qhc.frye.domain.DOrder;
+@Repository
+public interface DOrderRepository extends JpaRepository<DOrder, Integer> ,JpaSpecificationExecutor<DOrder>{
+
+}
---
+++ frye/src/main/java/com/qhc/frye/rest/controller/qhc/DOrderController.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.qhc.frye.rest.controller.qhc;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import com.qhc.frye.domain.DOrder;
+import com.qhc.frye.service.OrderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * @author lizuoshan
+ *
+ */
+@RestController
+@RequestMapping("dOrder")
+@Api(value = "DOrder management in Frye")
+public class DOrderController {
+	
+	
+	@Autowired
+	private OrderService orderService;
+	
+
+	
+	@ApiOperation(value="Get DOrder by id", notes="Get DOrder by id")
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public DOrder findByKOrderVersionId(Integer id) throws Exception
+    {	
+		
+		return orderService.findByKOrderVersionId(id);
+		
+    }
+	
+
+}
