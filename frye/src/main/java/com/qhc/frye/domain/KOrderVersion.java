e9eb3d46f4d4971907f7b6b41445303905e42e08
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-26 18:13:42 +0800

    update

--- frye/src/main/java/com/qhc/frye/domain/KOrderVersion.java
+++ frye/src/main/java/com/qhc/frye/domain/KOrderVersion.java
@@ -3,32 +3,16 @@ package com.qhc.frye.domain;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "k_order_version")
 public class KOrderVersion {
 
-	public enum Status {
-		Saved("00"), Submited("01"), Approved("02");
-
-		private final String flag;
-
-		private Status(String flag) {
-			this.flag = flag;
-		}
-
-		public String getCode() {
-			return flag;
-		}
-	}
 
 	@Id
 	@NotNull
@@ -40,9 +24,8 @@ public class KOrderVersion {
 	public String version;
 
 	@NotNull
-	@Column(name="status") 
-    @Enumerated(EnumType.STRING)
-	private String status;
+	@Column(name = "status", columnDefinition = "tinyint")
+	private Integer status;
 
 	@NotNull
 	@Column(name = "create_time", columnDefinition = "datetime")
@@ -51,6 +34,12 @@ public class KOrderVersion {
 	@NotNull
 	@Column(name = "k_orders_id")
 	private int kOrdersId;
+	
+	
+	
+	
+	
+	
 
 	public int getId() {
 		return id;
@@ -68,11 +57,11 @@ public class KOrderVersion {
 		this.version = version;
 	}
 
-	public String getStatus() {
+	public Integer getStatus() {
 		return status;
 	}
 
-	public void setStatus(String status) {
+	public void setStatus(Integer status) {
 		this.status = status;
 	}
 
