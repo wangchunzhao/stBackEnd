79239885c1af6a4be895b0f1842a9a33243a645a
wangch <chunzhao.wang@carrier.com>
2019-09-27 10:45:58 +0800

    update 11

--- frye/src/main/java/com/qhc/frye/rest/controller/qhc/BAreaController.java
+++ frye/src/main/java/com/qhc/frye/rest/controller/qhc/BAreaController.java
@@ -1,16 +1,26 @@
 package com.qhc.frye.rest.controller.qhc;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qhc.frye.domain.BArea;
 import com.qhc.frye.domain.BCity;
+import com.qhc.frye.domain.KOrderInfo;
+import com.qhc.frye.rest.controller.entity.PageHelper;
 import com.qhc.frye.service.BAreaService;
 import com.qhc.frye.service.BCityService;
 
@@ -25,13 +35,30 @@ public class BAreaController {
 	@Autowired
 	private BAreaService bAreaService;
 	
-	@ApiOperation(value="Add bCity", notes="Add bCity")
+	@ApiOperation(value="Add bArea", notes="Add bArea")
 	@PostMapping
     @ResponseStatus(HttpStatus.OK)
 	@ResponseBody
     public BArea add(@RequestBody(required=true) BArea bArea) throws Exception
     {	
 		return bAreaService.add(bArea);
+    }
+	
+	@ApiOperation(value=" Find all kOrderInfo paging info", notes="Find all kOrderInfo paging info")
+	@GetMapping("/paging")
+    @ResponseStatus(HttpStatus.OK)
+	public  PageHelper findPagingList(
+			@RequestParam("pageNo") int pageNo,
+			@RequestParam("pageSize") int pageSize,
+			@RequestParam("name") String name
+			) throws Exception{
+		Pageable pageable = PageRequest.of(pageNo, pageSize);
+		PageHelper pageHelper = new PageHelper();
+		
+		Page<List<Object>> page = bAreaService.getList(name, pageable);
+		pageHelper.setTotal(Integer.valueOf(page.getTotalElements()+""));
+		pageHelper.setRows(page.getContent());
+        return pageHelper;
     }
 
 }
--- frye/src/main/java/com/qhc/frye/service/BAreaService.java
+++ frye/src/main/java/com/qhc/frye/service/BAreaService.java
@@ -1,10 +1,15 @@
 package com.qhc.frye.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.qhc.frye.dao.BAreaRepository;
 import com.qhc.frye.domain.BArea;
+import com.qhc.frye.domain.KOrderInfo;
 
 @Service
 public class BAreaService {
@@ -14,6 +19,10 @@ public class BAreaService {
 	
 	public BArea add(BArea bArea) {
 		return bAreaRepository.save(bArea);
+	}
+	
+	public Page<List<Object>> getList(String name, Pageable pageable){
+		return bAreaRepository.findByName(name, pageable);
 	}
 
 }
