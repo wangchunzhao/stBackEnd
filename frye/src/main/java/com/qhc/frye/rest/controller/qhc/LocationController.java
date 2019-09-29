601ba84065f569959402e24d04dd4bba3c6d4621
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-29 16:17:49 +0800

    update

--- frye/src/main/java/com/qhc/frye/rest/controller/qhc/LocationController.java
+++ frye/src/main/java/com/qhc/frye/rest/controller/qhc/LocationController.java
@@ -3,30 +3,20 @@
  */
 package com.qhc.frye.rest.controller.qhc;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.qhc.frye.domain.SapSalesGroup;
-import com.qhc.frye.domain.SapSalesOffice;
 import com.qhc.frye.rest.controller.entity.SalesGroup;
 import com.qhc.frye.service.LocationService;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
