d743c5c53a5e244fe90d2a60dca09f6fc662f476
lizuoshan1028 <lizuoshan1028@163.com>
2019-10-08 09:28:47 +0800

    update

--- frye/src/main/java/com/qhc/frye/dao/ItemDetailRepository.java
+++ frye/src/main/java/com/qhc/frye/dao/ItemDetailRepository.java
@@ -3,10 +3,21 @@
  */
 package com.qhc.frye.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import com.qhc.frye.domain.ItemDetails;
+
 /**
- * @author wang@dxc.com
+ * @author lizuoshan
  *
  */
-public interface ItemDetailRepository {
+public interface ItemDetailRepository extends JpaRepository<ItemDetails, String> ,JpaSpecificationExecutor<ItemDetails> {
+	
+	@Query(value="select * from k_item_details where k_forms_id =?1" ,nativeQuery=true)
+	List<ItemDetails> findByKFormsId(String id);
 
 }
--- frye/src/main/java/com/qhc/frye/dao/SalesGroupRepository.java
+++ frye/src/main/java/com/qhc/frye/dao/SalesGroupRepository.java
@@ -1,21 +1,15 @@
-/**
- * 
- */
 package com.qhc.frye.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.qhc.frye.domain.SapSalesGroup;
-import com.qhc.frye.domain.SapSalesOffice;
 
 /**
- * @author wang@dxc.com
+ * @author 
  *
  */
 @Repository
 public interface SalesGroupRepository extends JpaRepository<SapSalesGroup, String> {
-	SapSalesOffice findByCode(String code);
 	
-
 }
