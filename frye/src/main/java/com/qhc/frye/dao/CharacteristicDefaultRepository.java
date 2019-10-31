8ca7ac491e06d9cd72729363b7fc475b6edb5a46
wangch <chunzhao.wang@carrier.com>
2019-10-31 15:52:40 +0800

    update 6

---
+++ frye/src/main/java/com/qhc/frye/dao/CharacteristicDefaultRepository.java
@@ -0,0 +1,9 @@
+package com.qhc.frye.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qhc.frye.domain.DCharacteristicDefault;
+
+public interface CharacteristicDefaultRepository extends JpaRepository<DCharacteristicDefault, String>{
+
+}
