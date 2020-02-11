dca8d4bfff619a157957fc001d1c34ffaaf151a0
Suzuwei <zuwei.su@dxc.com>
2020-02-11 09:31:07 +0800

    修改行项目错误属性名 重构订单毛利率计算逻辑

--- frye/src/main/java/com/qhc/order/service/GrossProfitMarginService.java
+++ frye/src/main/java/com/qhc/order/service/GrossProfitMarginService.java
@@ -3,6 +3,8 @@ package com.qhc.order.service;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,75 +34,100 @@ public class GrossProfitMarginService {
 		// 查询所有物料类型sap_material_group isenable != 0
 		List<MaterialGroups> groups = materialGroupsRepository.findByIsenableNotOrderByCode(0);
 		List<ItemDto> items = order.getItems();
+		double installFee = ObjectUtils.defaultIfNull(order.getInstallFee(), 0d);
+		double materialFee = ObjectUtils.defaultIfNull(order.getMaterialFee(), 0d);
+		double electricalFee = ObjectUtils.defaultIfNull(order.getElectricalFee(), 0d);
+		double maintenanceFee = ObjectUtils.defaultIfNull(order.getMaintenanceFee(), 0d);
+		double freight = ObjectUtils.defaultIfNull(order.getFreight(), 0d);
+		double additionalFreight = ObjectUtils.defaultIfNull(order.getAdditionalFreight(), 0d);
 
 		// 毛利率合计行
 		MaterialGroups sumMaterialGroup = new MaterialGroups();
 		sumMaterialGroup.setCode("sum");
 		sumMaterialGroup.setName("合计");
-		groups.add(sumMaterialGroup);
 
 		if (items == null || items.size() == 0) {
-			sumMaterialGroup(sumMaterialGroup, 0, 0, 0, 0, 0, 0);
+			setMaterialGroupMargin(sumMaterialGroup, 0, 0, 0, 0);
+			groups.add(sumMaterialGroup);
 
 			return groups;
 		}
 
+		// 合计值
 		double amount = 0;
 		double excludingTaxAmount = 0;
 		double cost = 0;
 		double wtwCost = 0;
-		double grossProfit = 0;
-		double wtwGrossProfit = 0;
 		for (MaterialGroups mgroup : groups) {
 			String code = mgroup.getCode();
 			String orderGroupCode = mgroup.getMaterialGroupOrderCode();
-			// 税率
-			Double taxRate = order.getTaxRate();
-
 			switch (code) {
 			case "3101": // "整机柜"
-				calcItemMargin(taxRate, mgroup, items);
+				calcItemMargin(mgroup, order);
 				break;
 			case "3102": // "分体柜"
-				calcItemMargin(taxRate, mgroup, items);
+				calcItemMargin(mgroup, order);
 				break;
 			case "3109": // "机组"
-				calcItemMargin(taxRate, mgroup, items);
+				calcItemMargin(mgroup, order);
 				break;
 			case "3103": // "换热器和冷凝器"
+				calcItemMargin(mgroup, order);
 				break;
 			case "3104": // "侧板"
+				calcItemMargin(mgroup, order);
 				break;
 			case "1000": // "散件"
+				calcItemMargin(mgroup, order);
 				break;
 			case "3105": // "冷库"
+				calcItemMargin(mgroup, order);
 				break;
 			case "3231": // "其他项目收费"
+				// 1元，固定值
+				setMaterialGroupMargin(mgroup, 0, 0, 1, 1);
 				break;
 			case "3212": // "安装费"
+				setMaterialGroupMargin(mgroup, 0, 0, installFee, installFee);
 				break;
 			case "3233": // "材料费"
+				setMaterialGroupMargin(mgroup, 0, 0, materialFee, materialFee);
 				break;
 			case "3235": // "电气费"
+				setMaterialGroupMargin(mgroup, 0, 0, electricalFee, electricalFee);
 				break;
 			case "3236": // "维保费"
+				setMaterialGroupMargin(mgroup, 0, 0, maintenanceFee, maintenanceFee);
 				break;
 			case "3234": // "销售运费"
+				setMaterialGroupMargin(mgroup, 0, 0, freight, freight);
 				break;
 			case "3237": // "不可预估费"
+				// 不可预估费是作为行项目录入，不在订单详情
+				calcItemMargin(mgroup, order);
 				break;
 			case "9103": // "追加运费"
+				setMaterialGroupMargin(mgroup, 0, 0, additionalFreight, additionalFreight);
 				break;
 			case "9101": // "B2C"
+				setMaterialGroupMargin(mgroup, 0, 0, 0, 0);
 				break;
 			case "9102": // "可选项"
+				setMaterialGroupMargin(mgroup, 0, 0, 0, 0);
 				break;
 			case "9999": // "不可用物料类别"
+				setMaterialGroupMargin(mgroup, 0, 0, 0, 0);
 				break;
 			}
+
+			amount += mgroup.getAmount().doubleValue();
+			excludingTaxAmount += mgroup.getExcludingTaxAmount().doubleValue();
+			cost += mgroup.getCost().doubleValue();
+			wtwCost += mgroup.getWtwCost().doubleValue();
 		}
 		// 合计行
-		sumMaterialGroup(sumMaterialGroup, amount, excludingTaxAmount, cost, wtwCost, grossProfit, wtwGrossProfit);
+		setMaterialGroupMargin(sumMaterialGroup, amount, excludingTaxAmount, cost, wtwCost);
+		groups.add(sumMaterialGroup);
 
 		return groups;
 	}
@@ -112,23 +139,20 @@ public class GrossProfitMarginService {
 	 * @param group
 	 * @param items
 	 */
-	private void calcItemMargin(double taxRate, MaterialGroups group, List<ItemDto> items) {
+	private void calcItemMargin(MaterialGroups group, OrderDto order) {
+		List<ItemDto> items = order.getItems();
+		double taxRate = order.getTaxRate();
 		String orderGroupCode = group.getMaterialGroupOrderCode();
 		String materialGroupCode = group.getCode();
 		double amount = 0;
 		double excludingTaxAmount = 0;
 		double cost = 0;
 		double wtwCost = 0;
-		double grossProfit = 0;
-		double grossProfitMargin = 0;
-		double wtwGrossProfit = 0;
-		double wtwGrossProfitMargin = 0;
 		for (ItemDto item : items) {
 			String itemMaterialGroupCode = item.getMaterialGroupCode();
 			if (itemMaterialGroupCode.equals(materialGroupCode)) {
-				// 1. 金额= sum（实卖金额合计），实卖金额=实卖价*数量，实卖价=零售价*折扣
-				double saleAmount = (item.getActualPrice() + item.getOptionalActualPrice() + item.getB2cEstimatedPrice()) * item.getQuantity();
-				amount += saleAmount;
+				// 1. 金额= sum（实卖金额合计），实卖金额=实卖价*数量，实卖价=零售价*折扣+可选项实卖价（差价）+b2c预估价
+				amount +=  (item.getActualPrice() + item.getOptionalActualPrice() + item.getB2cEstimatedPrice()) * item.getQuantity();
 				// 成本（销售）
 				cost += item.getTransationPrice() * item.getQuantity();
 				// 成本（生产）
@@ -138,23 +162,8 @@ public class GrossProfitMarginService {
 
 		// 2. 不含税金额=金额/(1+税率)
 		excludingTaxAmount = amount / (1 + taxRate);
-		// 毛利=不含税金额-成本
-		grossProfit = excludingTaxAmount - cost;
-		// 毛利（生产）=不含税金额-成本（生产）
-		wtwGrossProfit = excludingTaxAmount - wtwCost;
-		// 毛利率
-		grossProfitMargin = calcGrossProfitMargin(excludingTaxAmount, cost);
-		// WTW（生产）毛利率
-		wtwGrossProfitMargin = calcGrossProfitMargin(excludingTaxAmount, wtwCost);
 
-		group.setAmount(toBigDecimal(amount, 2));
-		group.setExcludingTaxAmount(toBigDecimal(excludingTaxAmount, 2));
-		group.setWtwCost(toBigDecimal(wtwCost, 2));
-		group.setCost(toBigDecimal(cost, 2));
-		group.setWtwGrossProfit(toBigDecimal(wtwGrossProfit, 2));
-		group.setGrossProfit(toBigDecimal(grossProfit, 2));
-		group.setWtwGrossProfitMargin(toBigDecimal(wtwGrossProfitMargin, 2).doubleValue());
-		group.setGrossProfitMargin(toBigDecimal(grossProfitMargin, 2).doubleValue());
+		setMaterialGroupMargin(group, amount, excludingTaxAmount, cost, wtwCost);
 	}
 
 	private BigDecimal toBigDecimal(double amount, int scale) {
@@ -162,30 +171,32 @@ public class GrossProfitMarginService {
 	}
 
 	/**
-	 * 添加合计行
+	 * 设置物料分组毛利率值
 	 * 
 	 * @param groups
 	 * @param amount
 	 * @param excludingTaxAmount
 	 * @param cost
 	 * @param wtwCost
-	 * @param grossProfit
-	 * @param wtwGrossProfit
 	 */
-	private void sumMaterialGroup(MaterialGroups group, double amount, double excludingTaxAmount, double cost,
-			double wtwCost, double grossProfit, double wtwGrossProfit) {
-		group.setAmount(toBigDecimal(amount, 2));
-		group.setExcludingTaxAmount(toBigDecimal(excludingTaxAmount, 2));
-		group.setWtwCost(toBigDecimal(wtwCost, 2));
-		group.setCost(toBigDecimal(cost, 2));
-		group.setWtwGrossProfit(toBigDecimal(wtwGrossProfit, 2));
-		group.setGrossProfit(toBigDecimal(grossProfit, 2));
-		// WTW毛利率
-		double wtwGrossProfitMargin = calcGrossProfitMargin(excludingTaxAmount, wtwCost);
-		// 毛利率
+	private void setMaterialGroupMargin(MaterialGroups mgroup, double amount, double excludingTaxAmount, double cost,
+			double wtwCost) {
+		// 毛利（销售）=不含税金额-成本（销售）
+		double grossProfit = excludingTaxAmount - cost;
+		// 毛利（生产）=不含税金额-成本（生产）
+		double wtwGrossProfit = excludingTaxAmount - wtwCost;
+		// 毛利率（销售）
 		double grossProfitMargin = calcGrossProfitMargin(excludingTaxAmount, cost);
-		group.setWtwGrossProfitMargin(wtwGrossProfitMargin);
-		group.setGrossProfitMargin(grossProfitMargin);
+		// 毛利率（生产）
+		double wtwGrossProfitMargin = calcGrossProfitMargin(excludingTaxAmount, wtwCost);
+		mgroup.setAmount(toBigDecimal(amount, 2));
+		mgroup.setExcludingTaxAmount(toBigDecimal(excludingTaxAmount, 2));
+		mgroup.setWtwCost(toBigDecimal(wtwCost, 2));
+		mgroup.setCost(toBigDecimal(cost, 2));
+		mgroup.setWtwGrossProfit(toBigDecimal(wtwGrossProfit, 2));
+		mgroup.setGrossProfit(toBigDecimal(grossProfit, 2));
+		mgroup.setWtwGrossProfitMargin(wtwGrossProfitMargin);
+		mgroup.setGrossProfitMargin(grossProfitMargin);
 	}
 
 	/**
@@ -195,11 +206,13 @@ public class GrossProfitMarginService {
 	 * @param cost   成本
 	 * @return
 	 */
-	private Double calcGrossProfitMargin(double amount, double cost) {
-		Double margin = 0D;
-		if (cost != 0) {
+	private double calcGrossProfitMargin(double amount, double cost) {
+		double margin = 0D;
+		if (amount != 0) {
 			margin = (amount - cost) / amount;
 		}
+		
+		margin = toBigDecimal(margin, 2).doubleValue();
 
 		return margin;
 	}
