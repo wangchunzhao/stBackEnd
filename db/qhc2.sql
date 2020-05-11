076305073c0d6aaa9b28edc9efdbf2ac63203a9f
Suzuwei <zuwei.su@dxc.com>
2020-05-11 10:40:41 +0800

    修改启动问题

--- db/qhc2.sql
+++ db/qhc2.sql
@@ -201,13 +201,14 @@ create table b_notify_infor
 /*==============================================================*/
 create table b_operate_log
 (
-   id                   integer auto_increment comment 'id',
+   id                   integer not null auto_increment comment 'id',
    operator             varchar(32) comment '操作人',
    operate_time         datetime comment '操作时间',
    operate_type         varchar(32) comment '操作类型',
    object_name          varchar(32) comment '操作对象',
    object_key           varchar(32) comment '操作对象关键信息',
-   remark               varchar(512) comment '操作描述'
+   remark               varchar(512) comment '操作描述',
+   primary key (id)
 );
 
 alter table b_operate_log comment '操作日志';
