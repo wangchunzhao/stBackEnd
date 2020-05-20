4385a37ca8472a74be4cb448e1e4f3adefb26658
wangch <chunzhao.wang@carrier.com>
2020-05-20 09:21:25 +0800

    修改定时任务

--- frye/src/main/java/com/qhc/sap/controller/SapSyncController.java
+++ frye/src/main/java/com/qhc/sap/controller/SapSyncController.java
@@ -107,6 +107,7 @@ public class SapSyncController {
 		currencyService.savePaymentPlan(lp);
 	}
 	
+//	@Scheduled(cron = "0 10 12 * * ?")
 	@ApiOperation(value = "同步sap的CharacteristicValue信息并写入销售工具数据库")
 	@GetMapping(value = "sycCharacteristicValue")
 	@ResponseStatus(HttpStatus.OK)
@@ -192,7 +193,7 @@ public class SapSyncController {
 	}
 	
 	//每天12点执行一次
-	@Scheduled(cron = "0 0 12 * * ?")
+//	@Scheduled(cron = "0 0 12 * * ?")
 	@ApiOperation(value = "同步sap的默认特征信息并写入销售工具数据库")
 	@GetMapping(value = "sycDefaultCharacteristic")
 	@ResponseStatus(HttpStatus.OK)
@@ -239,7 +240,7 @@ public class SapSyncController {
 	12.color
 	*/
 	//每天凌晨1点执行一次
-	@Scheduled(cron = "0 0 1 * * ?")
+//	@Scheduled(cron = "0 0 1 * * ?")
 	@ApiOperation(value = "同步sap数据进入销售工具")
 	@GetMapping(value = "SapToSellingTool")
 	@ResponseStatus(HttpStatus.OK)
